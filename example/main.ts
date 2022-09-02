b9e04f0481d60b7f124c03ce1c6a89706a377ca0
skanehira <sho19921005@gmail.com>
2022-09-03 05:40:21 +0900

    chore: update example

--- example/main.ts
+++ example/main.ts
@@ -1,4 +1,4 @@
-import { generateImage } from "../mod.ts";
+import * as silicon from "https://deno.land/x/silicon@v0.0.1/mod.ts";
 import { readAll } from "https://deno.land/std@0.153.0/streams/conversion.ts";
 
 const code = `package main
@@ -11,5 +11,5 @@ func main() {
     fmt.Println("Hello World")
 }`;
 
-const r = await generateImage(code, "go", { theme: "Dracula" });
+const r = await silicon.generateImage(code, "go", { theme: "Dracula" });
 await Deno.writeFile("out.png", await readAll(r));
