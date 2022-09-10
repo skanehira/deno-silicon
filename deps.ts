3716fe71326110679d84dedc37243dfafa25ed6c
skanehira <skanehira@users.noreply.github.com>
2022-09-10 00:03:47 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/deno-silicon/deno-silicon/deps.ts
    [1/4] Looking for releases: https://deno.land/std@0.154.0/testing/asserts.ts
    [1/4] Attempting update: https://deno.land/std@0.154.0/testing/asserts.ts -> 0.155.0
    [1/4] Update successful: https://deno.land/std@0.154.0/testing/asserts.ts -> 0.155.0
    [2/4] Looking for releases: https://deno.land/std@0.154.0/encoding/base64.ts
    [2/4] Attempting update: https://deno.land/std@0.154.0/encoding/base64.ts -> 0.155.0
    [2/4] Update successful: https://deno.land/std@0.154.0/encoding/base64.ts -> 0.155.0
    [3/4] Looking for releases: https://deno.land/std@0.154.0/io/mod.ts
    [3/4] Attempting update: https://deno.land/std@0.154.0/io/mod.ts -> 0.155.0
    [3/4] Update successful: https://deno.land/std@0.154.0/io/mod.ts -> 0.155.0
    [4/4] Looking for releases: https://deno.land/std@0.154.0/path/mod.ts
    [4/4] Attempting update: https://deno.land/std@0.154.0/path/mod.ts -> 0.155.0
    [4/4] Update successful: https://deno.land/std@0.154.0/path/mod.ts -> 0.155.0
    
    Successfully updated:
    https://deno.land/std@0.154.0/testing/asserts.ts 0.154.0 -> 0.155.0
    https://deno.land/std@0.154.0/encoding/base64.ts 0.154.0 -> 0.155.0
    https://deno.land/std@0.154.0/io/mod.ts 0.154.0 -> 0.155.0
    https://deno.land/std@0.154.0/path/mod.ts 0.154.0 -> 0.155.0

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * from "https://deno.land/std@0.154.0/testing/asserts.ts";
-export * as base64 from "https://deno.land/std@0.154.0/encoding/base64.ts";
-export * as io from "https://deno.land/std@0.154.0/io/mod.ts";
-export * as path from "https://deno.land/std@0.154.0/path/mod.ts";
+export * from "https://deno.land/std@0.155.0/testing/asserts.ts";
+export * as base64 from "https://deno.land/std@0.155.0/encoding/base64.ts";
+export * as io from "https://deno.land/std@0.155.0/io/mod.ts";
+export * as path from "https://deno.land/std@0.155.0/path/mod.ts";
