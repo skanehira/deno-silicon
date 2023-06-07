44d2486b7735b0f315f5c4e18f016d75e1628a9c
skanehira <sho19921005@gmail.com>
2023-06-07 22:45:56 +0900

    release: 0.0.6

--- bindings/bindings.ts
+++ bindings/bindings.ts
@@ -20,7 +20,7 @@ function readPointer(v: any): Uint8Array {
 }
 
 const url = new URL(
-  "https://github.com/skanehira/deno-silicon/releases/download/v0.0.5/",
+  "https://github.com/skanehira/deno-silicon/releases/download/v0.0.6/",
   import.meta.url,
 )
 
@@ -36,6 +36,11 @@ const opts: FetchOptions = {
     darwin,
     windows: uri,
     linux: uri,
+  },
+  suffixes: {
+    darwin: {
+      aarch64: "_arm64",
+    },
   },
   cache: "use",
 }
